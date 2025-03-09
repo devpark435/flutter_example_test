460287b3401eca84c2626d5c946ef645dc2994b0
devpark435 <devpark435@gmail.com>
2025-03-09 09:52:47 +0900

    에니메이션 관련 예제 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_example_test/animation/animation.dart';
 import 'package:flutter_example_test/todo/providers/todo_provider.dart';
 import 'package:flutter_example_test/todo/todo.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final todosProvider = TodosProvider();
-  await todosProvider.loadTodos(); // Todo 항목 불러오기
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => todosProvider,
-      child: const TodoApp(),
-    ),
-  );
+  // WidgetsFlutterBinding.ensureInitialized();
+  // final todosProvider = TodosProvider();
+  // await todosProvider.loadTodos(); // Todo 항목 불러오기
+  // runApp(
+  //   ChangeNotifierProvider(
+  //     create: (context) => todosProvider,
+  //     child: const TodoApp(),
+  //   ),
+  // );
+
+  runApp(const AnimationExampleApp());
 }
